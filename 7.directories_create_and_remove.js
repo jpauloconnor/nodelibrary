eeaf9c6dd202c83c57c18835236911b7fe3eb851
Paul <jamespauloconnor@gmail.com>
2016-07-27 19:54:17 -0400

    Create and remove a directory

---
+++ 7.directories_create_and_remove.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+
+//Deletes the file. 
+//fs.unlink('writeMe.txt');
+
+
+//Synchronous
+//Add a directory
+//fs.mkdirSync('stuff');
+
+//Remove a directory
+//fs.rmdirSync('stuff');
+
+//Build a dir with a file
+// fs.mkdir('stuff', function(){
+// 	fs.readFile('read.txt', 'utf8', function(err, data){
+// 		fs.writeFile('./stuff/write.txt', data);
+// 	});
+// });
+
+//Won't work. Can't remove without a dir
+//fs.rmdir('stuff');
+
+fs.unlink('./stuff/write.txt', function(){
+	fs.rmdir('stuff');
+});
+
