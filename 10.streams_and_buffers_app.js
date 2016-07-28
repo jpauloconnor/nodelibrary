9c8dc11d922b818be03a4d5d9aa825aad369687a
Paul <jamespauloconnor@gmail.com>
2016-07-27 20:19:22 -0400

    Streams and buffers

---
+++ 10.streams_and_buffers_app.js
@@ -0,0 +1,19 @@
+//BUFFER 
+//Temporary storage spot for a chunk of data that is being transferred from place to place
+
+								//Buffer
+//   Data passed <------------ |||||||||| <--- | ------  Big chunk of data
+
+//transfer a little at a time and transfer buffer 
+
+// Streams
+		//Data passed on and processed when buffer fills
+//   Data passed <---- |||||||||| -------- |||||||||| <--- | - | - | ------  Big chunk of data
+//Do it bit by bit. We can consume chunks 
+
+//RELEVANCE?
+
+//NODE can create streams for transfering data!!!!!
+
+//INCREASE PERFORMANCE
+
